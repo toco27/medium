08acccb36aad5b78077fcb224d8f7694ece2b14a
toco27 <42009557+toco27@users.noreply.github.com>
2018-12-30 14:43:09 +0100

    Create test.ts
    Test

---
+++ test.ts
@@ -0,0 +1,13 @@
+//test coding file
+import { Component, Input } from '@angular/core';
+import { Qualification } from './../../../matrix/shared/qualification.model';
+import { NGXLogger } from 'ngx-logger';
+import { QualificationService } from './../qualification.service';
+
+@Component({
+  selector: 'app-rating-qualification',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss']
+})
+export class RatingComponent {
+}
